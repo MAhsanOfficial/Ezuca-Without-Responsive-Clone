03f2041230a14b807c54c86453e48d43c70ab58e
Muhammad Ahsan <idahsanali@gmail.com>
2024-09-24 16:52:12 +0500

    first

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,235 @@
+import React from 'react';
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+    
+
+{/*  
+    <div className="hero-content">
+        <header className="site-header">
+          <div className="top-header-bar">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-12 col-lg-6 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
+                  <div className="header-bar-email d-flex align-items-center">
+                    <i className="fa fa-envelope"></i>
+                    <a href="#">
+                      <span className="__cf_email__" data-cfemail="96e2e3f7f8f8f7b8f2f3e5fff1f8d6f1fbf7fffab8f5f9fb">
+                        [email&#160;protected]
+                      </span>
+                    </a>
+                  </div>
+                  <div className="header-bar-text lg-flex align-items-center">
+                    <p><i className="fa fa-phone"></i>001-1234-88888</p>
+                  </div>
+                </div>
+                <div className="col-12 col-lg-6 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
+                  <div className="header-bar-search">
+                    <form className="flex align-items-stretch">
+                      <input type="search" placeholder="What would you like to learn?" />
+                      <button type="submit" className="flex justify-content-center align-items-center">
+                        <i className="fa fa-search"></i>
+                      </button>
+                    </form>
+                  </div>
+                  <div className="header-bar-menu">
+                    <ul className="flex justify-content-center align-items-center py-2 pt-md-0">
+                      <li><a href="#">Register</a></li>
+                      <li><a href="#">Login</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="nav-bar">
+            <div className="container">
+              <div className="row">
+                <div className="col-9 col-lg-3">
+                  <div className="site-branding">
+                    <h1 className="site-title">
+                      <a href="index.html" rel="home">Ezu<span>ca</span></a>
+                    </h1>
+                  </div>
+                </div>
+                <div className="col-3 col-lg-9 flex justify-content-end align-content-center">
+                  <nav className="site-navigation flex justify-content-end align-items-center">
+                    <ul className="flex flex-column flex-lg-row justify-content-lg-end align-content-center">
+                      <li className="current-menu-item"><a href="index.html">Home</a></li>
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Courses</a></li>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">Contact</a></li>
+                    </ul>
+                    <div className="hamburger-menu d-lg-none">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </div>
+                    <div className="header-bar-cart">
+                      <a href="#" className="flex justify-content-center align-items-center">
+                        <span aria-hidden="true" className="icon_bag_alt"></span>
+                      </a>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div> */}
+
+
+
+{/* 
+<div >
+        <header>
+          <div >
+            <div>
+              <div className="row">
+                <div>
+                  <div>
+                    <i></i>
+                    <a href="#">
+                      <span>
+                        [email&#160;protected]
+                      </span>
+                    </a>
+                  </div>
+                  <div>
+                    <p><i ></i>001-1234-88888</p>
+                  </div>
+                </div>
+                <div >
+                  <div>
+                    <form>
+                      <input type="search" placeholder="What would you like to learn?" />
+                      <button type="submit">
+                        <i></i>
+                      </button>
+                    </form>
+                  </div>
+                  <div>
+                    <ul >
+                      <li><a href="#">Register</a></li>
+                      <li><a href="#">Login</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div >
+                    <h1>
+                      <a href="index.html" rel="home">Ezu<span>ca</span></a>
+                    </h1>
+                  </div>
+                </div>
+                <div>
+                  <nav >
+                    <ul >
+                      <li ><a href="index.html">Home</a></li>
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Courses</a></li>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">Contact</a></li>
+                    </ul>
+                    <div >
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </div>
+                    <div>
+                      <a href="#">
+                        <span aria-hidden="true"></span>
+                      </a>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div> */}
+
+
+
+
+
+
+
+
+
+<div class="hero-content">
+  <header class="site-header">
+    <div class="top-header-bar">
+      <div class="container">
+        <div class="header-row">
+          <div class="header-left">
+            <div class="header-bar-email">
+              <i class="fa fa-envelope"></i>
+              <a href="idahsanali@gmail.com">idahsanali@gmail.com</a>
+            </div>
+            <div class="header-bar-phone">
+              <i class="fa fa-phone"></i>
+              <p>92-47-1777-414</p>
+            </div>
+          </div>
+          <div class="header-right">
+            <div class="header-bar-search">
+              <form>
+                <input type="search" placeholder="What would you like to learn?" />
+                <button type="submit">
+                  <i class="fa fa-search"></i>
+                </button>
+              </form>
+            </div>
+            <div class="header-bar-menu">
+              <ul>
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Login</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="nav-bar">
+      <div class="container">
+        <div class="nav-row">
+          <div class="site-branding">
+            <h1 class="site-title">
+              <a href="index.html" rel="home">Ezu<span>ca</span></a>
+            </h1>
+          </div>
+          <nav class="site-navigation">
+            <ul>
+              <li class="current-menu-item"><a href="index.html">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Courses</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</div>
+
+
+
+
+    </>
+  );
+}
+
+export default Navbar;
